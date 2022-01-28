5ead7353fb155f45916716ed4103acfbff5901a6
Dalton <daltonkajander@knights.ucf.edu>
2022-01-27 21:04:41 -0500

    Basic Multithreading Added

--- Assignment/ThreadedPrimes.cpp
+++ Assignment/ThreadedPrimes.cpp
@@ -6,30 +6,39 @@
 #include <cmath>
 #include <chrono>
 
-int main() {
-	// Amount of numbers to go through
-	int n;
-	n = 1e8;
+bool locked = true;
+// Constants
+int n = 1e8;
+int nSqrt = ceil(sqrt(n));
+int threadCount = 8;
 
+void worker(int tid, bool* primes);
+
+int main() {
 	// Prime array, all set to true by default
 	bool* primes = (bool*)malloc(sizeof(bool) * n);
 	for (int i = 0; i < n; i++) {
 		primes[i] = true;
 	}
+	primes[0] = false;
+	primes[1] = false;
+
+	// Create threads
+	std::thread threads[threadCount];
+	for (int i = 0; i < threadCount; i++) {
+		threads[i] = std::thread(worker, i, primes);
+	}
+
+	locked = false;
+	
+	for (int i = 0; i < threadCount; i++) {
+		threads[i].join();
+	}
+
+	std::cout << "Threads done\n";
 
 	// Start timer
 	auto start = std::chrono::high_resolution_clock::now();
-
-	// Sieve through
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (primes[i]) {
-			int j = i * i, k = 0;
-			while (j < n) {
-				primes[j] = false;
-				j = (i * i) + (k++ * i);
-			}
-		}
-	}
 	auto stop = std::chrono::high_resolution_clock::now();
 	float time = (std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()) / 1000.0;
 
@@ -49,4 +58,24 @@ int main() {
 	// Free and end
 	free(primes);
 	return 0;
+}
+
+void worker(int tid, bool* primes) {
+	while (locked) {
+		continue;
+	}
+	int start, stop;
+	start = tid * (nSqrt / threadCount);
+	stop = ((nSqrt * (tid + 1)) / threadCount) - 1;
+	std::cout << "Unlocked thread " + std::to_string(tid)+ " with a range of " + std::to_string(start) + " to " + std::to_string(stop) + "\n";
+
+	for (int i = start; i <= stop; i++) {
+		if (primes[i]) {
+			int j = i * i, k = 0;
+			while (j < n) {
+				primes[j] = false;
+				j = (i * i) + (k++ * i);
+			}
+		}
+	}
 }
