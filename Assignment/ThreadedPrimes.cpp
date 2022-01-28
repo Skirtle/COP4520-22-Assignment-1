0deacd5921f7eca42c2e6f980c5e84b066ad099f
Dalton <daltonkajander@knights.ucf.edu>
2022-01-27 20:03:15 -0500

    Elapsed time

--- Assignment/ThreadedPrimes.cpp
+++ Assignment/ThreadedPrimes.cpp
@@ -4,15 +4,23 @@
 #include <functional>
 #include <vector>
 #include <cmath>
+#include <chrono>
 
 int main() {
+	// Amount of numbers to go through
 	int n;
 	n = 1e8;
+
+	// Prime array, all set to true by default
 	bool* primes = (bool*)malloc(sizeof(bool) * n);
 	for (int i = 0; i < n; i++) {
 		primes[i] = true;
 	}
 
+	// Start timer
+	auto start = std::chrono::high_resolution_clock::now();
+
+	// Sieve through
 	for (int i = 2; i <= sqrt(n); i++) {
 		if (primes[i]) {
 			int j = i * i, k = 0;
@@ -22,17 +30,21 @@ int main() {
 			}
 		}
 	}
+	auto stop = std::chrono::high_resolution_clock::now();
+	float time = (std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()) / 1000.0;
 
-	std::cout << "Sieve done" << std::endl;
-
+	// Sum all primes
 	unsigned long long sum = 0;
+	int numPrimes = 0;
 	for (int i = 2; i < n; i++) {
 		if (primes[i]) {
 			sum += i;
+			numPrimes++;
 		}
 	}
-	std::cout << sum << std::endl;
+	std::cout << time << " " << numPrimes << " " << sum << std::endl;
 
+	// Free and end
 	free(primes);
 	return 0;
 }
